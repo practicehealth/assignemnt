335db1b778e0360fc8e51437776c48176a055a99
shivakr07 <kumarshiva0707@gmail.com>
2023-09-10 15:02:32 +0530

    completed components

---
+++ client/src/components/Calender.tsx
@@ -0,0 +1,30 @@
+
+const months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const days = [ 'Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
+const suffix = [ 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th', 'th'  ];
+function Calender( { data } : { data: string } ) {
+
+    const dateVal = new Date(data);
+    
+    const year = dateVal.getUTCFullYear()
+    const mnth = dateVal.getMonth();
+    const date = dateVal.getDate();
+    const day = dateVal.getDay();
+    let suff;
+    if ( 11<=date && date<=13 ) suff = 'th';
+    else suff = suffix[date%10-1];
+
+    return (
+        <div className=" w-36 text-center border shadow rounded-lg overflow-hidden">
+            <div className="h-6 bg-accent text-center w-full">
+                { year }
+            </div>
+            <div className="flex flex-col h-full">
+                <p className="text-xl font-semibold py-2">{months[mnth]}</p>
+                <p className="font-medium py-1">{date}{suff } {days[day].slice(0,3)}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Calender
